3ebf31ebceb169e3dbeee478d00b39addeadc93b
TimDev14 <njagi.timothy14@gmail.com>
2026-06-26 09:21:15 +0300

    Update Pipfile and Pipfile.lock for Python version compatibility; implement response handling in GetRequester

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -1,13 +1,13 @@
 import requests
 import json
-
 class GetRequester:
 
     def __init__(self, url):
         self.url = url
 
     def get_response_body(self):
-        pass
+        self.respose = requests.get(self.url)
 
     def load_json(self):
-        pass
+        self.data = self.respose.json()
+        return self.data
